c26fa8e7684128011442f4b380273cf8be0e7533
Jonathan Graham <jgraham@8thlight.com>
2016-03-15 15:34:08 -0500

    refactor calculate array length from grid size using square function

--- src/board.js
+++ src/board.js
@@ -1,11 +1,16 @@
 function create_new_board(grid_size) {
 	grid_size = grid_size || 3;
 	var board = [],
-		array_size = grid_size * grid_size;
+		array_size = square(grid_size);
 	for (i = 0; i < array_size; i += 1) {
 		board.push(" ");
 	} 
 	return board;
+}
+
+function square(number) {
+	var square = number * number;
+	return square;
 }
 
 exports.create_new_board = create_new_board;
