f5fa854d1d72104c3857c7bea3a0da5c604a9b53
Jonathan Graham <jon@Jonathans-MacBook-Pro.local>
2016-03-15 15:29:10 -0500

    create new boards based on grid-size

--- spec/test_board.js
+++ spec/test_board.js
@@ -10,3 +10,26 @@ describe('creates a default new board', function () {
     assert.deepEqual([" ", " ", " ", " ", " ", " ", " ", " ", " "], new_board);
   });
 });
+
+describe('creates a new board with input grid-size', function () {
+
+  it('should return a nine-element array with grid-size of 3', function () {
+    var new_board = board.create_new_board(3);
+    assert(Array.isArray(new_board));
+    assert.equal(9, new_board.length);
+    assert.deepEqual([" ", " ", " ", " ", " ", " ", " ", " ", " "], new_board);
+  });
+
+  it('should return a four-element array with grid-size of 2', function () {
+    var new_board = board.create_new_board(2);
+    assert(Array.isArray(new_board));
+    assert.equal(4, new_board.length);
+    assert.deepEqual([" ", " ", " ", " "], new_board);
+  });
+
+  it('should return a 16-element array with grid-size of 4', function () {
+    var new_board = board.create_new_board(4);
+    assert(Array.isArray(new_board));
+    assert.equal(16, new_board.length);
+  });
+});
--- src/board.js
+++ src/board.js
@@ -1,5 +1,11 @@
-function create_new_board() {
-	return [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+function create_new_board(grid_size) {
+	grid_size = grid_size || 3;
+	var board = [],
+		array_size = grid_size * grid_size;
+	for (i = 0; i < array_size; i += 1) {
+		board.push(" ");
+	} 
+	return board;
 }
 
 exports.create_new_board = create_new_board;
