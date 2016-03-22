aaf04078936158f6b8046822a91f7f2915db76e3
Jonathan Graham <jgraham@8thlight.com>
2016-03-22 16:28:55 -0500

    refactor random determine_move

--- src/players/random.js
+++ src/players/random.js
@@ -1,11 +1,12 @@
-var board = require('../board');
-
-function determine_move(b) {
-	var move = Math.floor(Math.random() * b.length);
-	if (board.valid_space(b, move)) {
-		return move;
-	}
-	return determine_move(b);
+function determine_move(board) {
+	var spaces;
+	spaces = board.map(function(value, index){
+				return value === " " ? index : value
+			 }).filter(function(map_value) {
+				return (typeof(map_value) === 'number')
+			 });
+	random = Math.floor(Math.random() * spaces.length);
+	return spaces[random];	
 }
 
 exports.determine_move = determine_move;
